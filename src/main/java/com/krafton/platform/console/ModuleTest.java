5b747e106b44e97af51ad453bfff4fa341175b48
Brand Baek <kaeskers@gmail.com>
2019-03-09 13:43:03 +0900

    initialize

---
+++ src/main/java/com/krafton/platform/console/ModuleTest.java
@@ -0,0 +1,12 @@
+package com.krafton.platform.console;
+
+/**
+ * Created by sgbaek on 2019-03-09.
+ */
+public class ModuleTest {
+
+    @Override
+    public String toString() {
+        return "Module";
+    }
+}
